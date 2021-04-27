acb5e1ecdfb5ecf7c1d28807e051474beffa6347
jxk5861 <jxk5861@psu.edu>
2021-04-27 17:08:38 -0400

    Idea for cool drawing method using modulus.

--- MandelbrotBackgroundCreator/src/karabin/mandelbrot/drawing/DrawingMethod.java
+++ MandelbrotBackgroundCreator/src/karabin/mandelbrot/drawing/DrawingMethod.java
@@ -26,7 +26,16 @@ public abstract class DrawingMethod implements DrawingMethodIF {
 			return 0;
 		}
 
+		
 		double factor = rate / this.iterations;
+		
+		// normalized escape with mods and alternating color
+//		Color color = this.color;
+//		if((int)(rate / 255) % 2 == 1) {
+//			color = new Color(255, 255, 100);
+//		}
+//		double factor = (rate % 255) / 255;
+
 		
 		return new Color((int)(factor * color.getRed()), (int) (factor * color.getGreen()), (int)(factor * color.getBlue())).getRGB();
 	}
