175d0464bc7ebb8d4013a085eadd200ef971f3e5
Jacob Karabin <jjkarabin@gmail.com>
2021-11-21 14:08:46 -0500

    Julia set stuff in comments

--- MandelbrotBackgroundCreator/src/karabin/mandelbrot/drawing/mulithreaded/mandelbrot/MandelbrotHistogramSingleColor.java
+++ MandelbrotBackgroundCreator/src/karabin/mandelbrot/drawing/mulithreaded/mandelbrot/MandelbrotHistogramSingleColor.java
@@ -14,8 +14,15 @@ public class MandelbrotHistogramSingleColor extends HistogramSingleColor {
 
 	@Override
 	protected double escape(Complex c) {
-		double real = 0;
-		double image = 0;
+		double real = 0;//c.getReal();
+		double image = 0;//c.getImaginary();
+		
+//		Complex c2 = new Complex(-0.8, 0.156);
+//		Complex c2 = new Complex(-0.4, 0.6);
+//		Complex c2 = new Complex(0, -0.8);
+//		Complex c2 = new Complex(-0.7269, 0.1889);
+//		double alpha = 2*Math.PI * 0.7;
+//		Complex c2 = new Complex(0.7885).multiply(Complex.valueOf(Math.E).pow(Complex.valueOf(0, alpha)));
 
 		for (int i = 0; i < iterations; i++) {
 			double s1 = real * real;
@@ -24,8 +31,8 @@ public class MandelbrotHistogramSingleColor extends HistogramSingleColor {
 //				normalizedRates[x][y] = i + 1 - Math.log10(Math.log(s1 + s2) / Math.log(2));
 				return i + 1 - Math.log10(Math.log(s1 + s2) / Math.log(2));
 			}
-			double real2 = s1 - s2 + c.getReal();
-			double image2 = 2 * real * image + c.getImaginary();
+			double real2 = s1 - s2 + c.getReal(); //c2 for julia
+			double image2 = 2 * real * image + c.getImaginary(); //c2 for julia
 			real = real2;
 			image = image2;
 		}
